88e77f1a04f923698260fad7e1e8d7b1f367538c
Mohamed14-7 <96206856+Mohamed14-7@users.noreply.github.com>
2025-03-02 16:14:42 +0200

    task_2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
-
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -27,4 +27,14 @@ Route::post('/about', function () {
         '03' => 'Sales',
     ];
     return view('about', compact('name', 'departments'));
+});
+
+Route::get('tasks', function () {
+    return view('tasks');
+});
+Route::post('create', function () {
+    $name = $_POST['name'];
+
+    DB::table('tasks')->insert(['name' => $name]);
+    return view('tasks');
 });
